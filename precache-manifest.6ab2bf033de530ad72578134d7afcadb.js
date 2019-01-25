682d6c9b41391d5b3144128fc1a8ce89f3a2691b
Tony <TTuretsky@gmail.com>
2019-01-25 13:55:32 -0500

    Updates

---
+++ precache-manifest.6ab2bf033de530ad72578134d7afcadb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15013f1e1722c6d044dd",
+    "url": "/hookers/static/js/runtime~main.15013f1e.js"
+  },
+  {
+    "revision": "d75aaccb4f560e753b50",
+    "url": "/hookers/static/js/main.d75aaccb.chunk.js"
+  },
+  {
+    "revision": "18a1c637d7805cd07ce3",
+    "url": "/hookers/static/js/1.18a1c637.chunk.js"
+  },
+  {
+    "revision": "d75aaccb4f560e753b50",
+    "url": "/hookers/static/css/main.97afdbf6.chunk.css"
+  },
+  {
+    "revision": "ca1bea747135c100524eaca2271c47f8",
+    "url": "/hookers/index.html"
+  }
+];
